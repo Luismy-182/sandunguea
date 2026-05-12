0b806f9e6e267d4a20308db8cfd070ec984cba32
Miguel Angel Suarez <miguel.buny@gmail.com>
2026-05-12 13:23:59 -0600

    mejorando la seccion, asi se vive sandunguea

--- components/sandunguea.php
+++ components/sandunguea.php
@@ -21,27 +21,33 @@
         <!-- VIDEO PRINCIPAL -->
         <div class="video-card video-main">
             <div class="video-placeholder">
-                <video autoplay muted loop controls>
-                    <source src="/assets/videos/videoHero.mp4" type="video/mp4" loading="lazy">
+                <video autoplay muted loop playsinline preload="auto">
+                    <source src="/assets/videos/videoHero.mp4" type="video/mp4">
                 </video>
             </div>
+            <div class="video-label">Clase grupal 💃</div>
+            <p class="video-parrafo">Ambiente, energía y buena vibra</p>
         </div>
 
         <!-- SECUNDARIOS -->
         <div class="video-card">
             <div class="video-placeholder">
-                <video autoplay muted loop controls>
-                    <<source src="/assets/videos/2.1.mp4" type="video/mp4" loading="lazy">
+                <video autoplay muted loop playsinline preload="metadata">
+                    <source src="/assets/videos/2.1.mp4" type="video/mp4">
                 </video>
             </div>
+            <div class="video-label">Tu confianza, primero 💃</div>
+            <p class="video-parrafo">Avanzas cuando estás listo.</p>
         </div>
 
         <div class="video-card">
             <div class="video-placeholder">
-                <video autoplay muted loop controls>
-                    <source src="/assets/videos/complemento2.mp4" type="video/mp4" loading="lazy">
+                <video autoplay muted loop playsinline preload="metadata">
+                    <source src="/assets/videos/complemento2.mp4" type="video/mp4">
                 </video>
             </div>
+            <div class="video-label">Otro nivel, otra vibra 💃</div>
+            <p class="video-parrafo">Distintos estilos, misma familia</p>
         </div>
 
     </div>
