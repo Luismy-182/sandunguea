1af8d2f3ed12ed5b0a7ea06bb3c495222ca03386
Miguel Angel Suarez <miguel.buny@gmail.com>
2026-05-25 17:55:09 -0600

    ajustes finales a landing page

--- components/footer.php
+++ components/footer.php
@@ -1,8 +1,25 @@
 <footer class="footer">
-    
+
     <!-- FOOTER -->
     <div class="sandunguea-footer ">
-        <em class="logo-glow sandunguea-letter ">Sandunguea</em>
+        <em class="sandunguea-letter ">Sandunguea</em>
+    </div>
+    <div class="footer-redes">
+
+        <a href="https://instagram.com/sandunguea_123.567">
+            Instagram
+        </a>
+        <span>•</span>
+
+        <a href="https://tiktok.com/@sandunguea4">
+            TikTok
+        </a>
+        <span>•</span>
+
+        <a href="https://www.facebook.com/profile.php?id=61562104610190">
+            Facebook
+        </a>
+
     </div>
     <p>© <?php echo date('Y'); ?> Sandunguea – Todos los derechos reservados</p>
     <p style="margin-top:.4rem;">Hecho con 💃 y mucho ritmo</p>
