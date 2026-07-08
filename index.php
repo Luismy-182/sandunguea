725dde9874afda5804b616f69d86c9da82a36654
Miguel Angel Suarez <miguel.buny@gmail.com>
2026-07-08 17:24:40 -0600

    se agregan nuevas imagenes de cierre

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sandunguea - Escuela de baile</title>
-    <link rel="shortcut icon" href="/assets/img/zapatilla2.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="/assets/img/logoIcon.svg" type="image/x-icon">
     <link
         rel="preload"
         href="/assets/fonts/MoonTime-Regular.woff2"
